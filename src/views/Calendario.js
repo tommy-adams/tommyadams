d7e31e2cb2d1a4e4b1184139069a4b83bfd92f38
Thomas Adams <thomasadams012@gmail.com>
2021-08-31 15:24:34 -0400

    redirect without auth

--- src/views/Calendario.js
+++ src/views/Calendario.js
@@ -3,6 +3,10 @@ import ActionBar from "src/components/Calendario/ActionBar";
 import Calendar from "src/components/Calendario/Calendar";
 
 const Calendario = () => {
+  const token = sessionStorage.getItem("token");
+
+  if (!!!token) window.location.pathname = "/clogin";
+
   return (
     <div className="w-screen">
       <ActionBar />
